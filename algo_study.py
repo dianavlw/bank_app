7cbd3aa0c334c102c6f91904e695f8dd3df0ed1e
Diana Vargas <dianavlw@gmail.com>
2025-04-15 20:35:45 -0700

    list of problems

---
+++ algo_study.py
@@ -0,0 +1,96 @@
+"""
+Sliding Window
+Leetcode Problems:
+53. Maximum Subarray 
+643. Maximum Average Subarray I
+3. Longest Substring without repeating characters
+76. Minimum Window substring
+
+
+example: Find the longers substring without dupes
+
+Two Pointers
+LeetCode Problems:
+167. Two SumII - Input Array is Sorted
+15. 3 Sum 
+11. Container With Most Water
+
+Prefix Sum
+LeetCode Problems:
+
+303.Range Sum Query- Immutable
+525. Contigous Array
+560.Subarray Sum Equals K
+
+Linked List & Arrays
+-Cycles
+
+Fast and Slow pointers
+Leetcode Problems:
+141. Linked list Cycle
+202. Happy Number
+287. Find the Duplicate Number
+
+Linked List in Place Reversal
+Leetcode Problems:
+206.Reverse Linked List
+92. Reverse Linked List II
+24. Swap Nodes in Paris
+
+Monotonic Stack
+Leetcode Problems:
+496. Next Greater Element
+739. Daily Temperatures
+84. Largest Rectangle in Histogram
+
+Top 'K' Elements
+Leetcode Problems:
+215. kth Largest Element in an Array
+347. Top K Frequent Elements
+373. Find K Pairs with Smallest Sums
+
+Overlapping Intervals
+56. Merge Intervals
+57. Insert Interval
+435. Non-overlapping intervals
+
+Modified Binary Search
+33. Search in Rotated Sorted Array
+153. Find Minimum in Rotated Sorted Array
+240. Search a 2D Matrix II
+
+Binary Tree Traversal
+257. Binary Tree Paths
+230. Kth Smallest Element in a BST
+124. Binary Tree Maximum Path Sum
+107. Binary Tree Level Order Traversal II
+
+Depth First Search
+133. Clone Graph
+113. Path Sum II
+210. Course Schedule II
+
+Breadth First Search
+102. Binary Tree Level Order Traversal
+994. Rotting Oranges
+127. Word Ladder
+
+Matrix Traversal
+733. Flood Fill
+200. Number of Islands
+130. Surrounded Regions
+
+Backtracking
+46. Permutations
+78. Subsets
+51. N-Queens
+
+Dynamic Programming
+70. Climbing Stairs
+322. Coin Change
+300. Longest Increasing Subsequence
+416. Partition Equal Subset Sum
+312. Burst Balloons
+1143. Longest Common Subsequence
+
+"""
